e7c1dc24a0f4b84b34d6a53de6473f9b51856b36
Vladishar <shargvladi@gmail.com>
2025-01-04 23:29:59 +0200

    fix build script 2

--- build.bat
+++ build.bat
@@ -1,5 +1,12 @@
+del bin\PEGo.exe
+
+go-winres make -in src\winres\winres.json -out src\rsrc
+@REM go-winres make -in src\winres\winres.json
+
+
 @REM this command builds without console window
-@REM go build -ldflags="-H=windowsgui -s -w" -o bin\PEGo.exe .
-go-winres make -in src\winres\winres.json
-go build -ldflags="-s -w" -o bin\PEGo.exe .\src
+go build -ldflags="-H=windowsgui -s -w" -o bin\PEGo.exe .\src
+
+@REM this command builds with console window
+@REM go build -ldflags="-s -w" -o bin\PEGo.exe .\src
 
